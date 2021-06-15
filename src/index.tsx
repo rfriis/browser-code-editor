418491422d2f7ef95a5927d33c438036f72f8f40
Ruben Friis Ronne <rubenfriis@gmail.com>
2021-06-15 17:52:59 +0100

    Added UNPKG to fetch NPM modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -18,16 +19,25 @@ const App = () => {
   }, []);
 
   const onClick = async () => {
+    // check for esbuild
     if (!ref.current) {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: 'jsx',
-      target: 'es2015',
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
-    setCode(result.code);
+    // console.log(result);
+
+    setCode(result.outputFiles[0].text);
   };
 
   return (
