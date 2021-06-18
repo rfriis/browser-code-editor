0b4737d1cadc5279b7a8085310ca6d2c2635b5d2
Ruben Friis Ronne <rubenfriis@gmail.com>
2021-06-18 12:32:47 +0100

    added monaco editor and bulmaswatch css

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import * as esbuild from 'esbuild-wasm';
 import ReactDOM from 'react-dom';
 import { useState, useEffect, useRef } from 'react';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
+import CodeEditor from './components/code-editor';
 
 const App = () => {
   const ref = useRef<any>();
@@ -63,6 +65,10 @@ const App = () => {
 
   return (
     <div>
+      <CodeEditor
+        initialValue="const App = () => {};"
+        onChange={(value) => setInput(value)}
+      />
       <textarea
         value={input}
         onChange={(e) => setInput(e.target.value)}
