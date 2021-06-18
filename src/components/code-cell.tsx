06dd93b8b6d1f93a85eace052c748ead0a8e0b92
Ruben Friis Ronne <rubenfriis@gmail.com>
2021-06-18 16:44:35 +0100

    horizontal resizing of code cell

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -16,10 +16,12 @@ const CodeCell = () => {
   return (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
-        <CodeEditor
-          initialValue="const App = () => {};"
-          onChange={(value) => setInput(value)}
-        />
+        <Resizable direction="horizontal">
+          <CodeEditor
+            initialValue="const App = () => {};"
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
         <Preview code={code} />
       </div>
     </Resizable>
